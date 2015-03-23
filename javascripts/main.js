ff41c777ca77312e4b62c38ccb2762ccbe0c993b
HomemadeIcing <alanng95@hotmail.com>
2015-03-23 13:37:59 +1100

    deleted soem stuff

--- javascripts/main.js
+++ javascripts/main.js
@@ -22,14 +22,48 @@ $(document).ready(function() {
 
 		// Tree Menu
 			// First Level Menu
-			$('.accordian > ul').hide();
-			$('.accordian-first:first-child').siblings().hide();
+				
+$("#selector").jstree();
+			// $('.accordian > ul').hide();
+			// $('.accordian-first:first-child').siblings().hide();
 
-			$('.plus-minus-toggle').click(function(){		
-				$(this).parent().parent().find('ul').slideToggle('fast');
-			}); 
-			// Second Level Menu							
-			$('.accordian-first .plus-minus-toggle').click(function(){				
-				$(this).parent().parent().find('li:not(:first-child)').slideToggle(100);
-			});
+			// $('.plus-minus-toggle').click(function(){		
+			// 	$(this).parent().parent().find('ul').slideToggle('fast');
+			// }); 
+			// // Second Level Menu							
+			// $('.accordian-first .plus-minus-toggle').click(function(){				
+			// 	$(this).parent().parent().find('li:not(:first-child)').slideToggle(100);
+			// });
+   //    $(".left-hand-menu").jstree({
+   //        "json_data" : {
+   //            "data":[
+   //                {
+   //                    "data" : "Search engines",
+   //                    "children" :[
+   //                                 {"data":"Yahoo", "metadata":{"href":"http://www.yahoo.com"}},
+   //                                 {"data":"Bing", "metadata":{"href":"http://www.bing.com"}},
+   //                                 {"data":"Google", "children":[{"data":"Youtube", "metadata":{"href":"http://youtube.com"}},{"data":"Gmail", "metadata":{"href":"http://www.gmail.com"}},{"data":"Orkut","metadata":{"href":"http://www.orkut.com"}}], "metadata" : {"href":"http://youtube.com"}}
+   //                                ]
+   //                },
+   //                {
+   //                    "data" : "Networking sites",
+   //                    "children" :[
+   //                        {"data":"Facebook", "metadata":{"href":"http://www.fb.com"}},
+   //                        {"data":"Twitter", "metadata":{"href":"http://twitter.com"}}
+   //                    ]
+   //                }
+   //            ]
+   //        },
+   //        "plugins" : [ "themes", "json_data", "ui" ]
+   //    }).bind("select_node.jstree", function(e, data)
+   //        {
+   //            if(jQuery.data(data.rslt.obj[0], "href"))
+   //            {
+   //                window.location=jQuery.data(data.rslt.obj[0], "href");
+   //            }
+   //            else
+   //            {
+   //                alert("No href defined for this element");
+   //            }
+   //        });
 });			
