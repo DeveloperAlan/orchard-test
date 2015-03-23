95e818b6dcec0207f338f644a3324d1616f0f2f1
HomemadeIcing <alanng95@hotmail.com>
2015-03-23 18:48:40 +1100

    done

--- javascripts/main.js
+++ javascripts/main.js
@@ -1,5 +1,10 @@
 $(document).ready(function() {
 		// Tree Menu
 
-$("#selector").jstree();
+	$("#selector").jstree();
+	jstree.bind("click.jstree", function (event) {
+   var node = $(event.target).closest("li");
+   var data = node.data("jstree");
+   // Do some action
+});
 });			
